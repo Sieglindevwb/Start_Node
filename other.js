c900601e7966f6dc63a9eca34cbe6a0f5cc5071f
Sieglinde <sieglindevwb@gmail.com>
2023-12-12 12:33:48 +0100

    Add new excercise (req, res)

---
+++ other.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
+//creating files
+try {
+  const indexFilePath = path.join(__dirname, 'index.html');
+  fs.writeFileSync(
+    indexFilePath,
+    '<!DOCTYPE html><html><head><title>Homepage Client</title><link rel= "stylesheet" href= "style.css"></head><body><h1>Hello, World!</h1></body></html>'
+  );
+  console.log('Succesfully created index page');
+
+  const cssFilePath = path.join(__dirname, 'style.css');
+  fs.writeFileSync(cssFilePath, 'body {background-color: pink}');
+  console.log('Succefully created css file');
+} catch (error) {
+  console.error('Error creating files', error);
+}
+
+const contactFolderPath = path.join(__dirname, 'contact');
+try {
+  const indexFilePath = path.join(contactFolderPath, 'index.html');
+  fs.writeFileSync(
+    indexFilePath,
+    '<!DOCTYPE html><html><head><title>Contact page</title><link rel= "stylesheet" href= "style.css"></head><body><h1>Hello, Contact here!</h1></body></html>'
+  );
+  console.log('Succesfully created index page in the contact folder');
+
+  const cssFilePath = path.join(contactFolderPath, 'style.css');
+  fs.writeFileSync(cssFilePath, 'body {background-color: blue}');
+  console.log('Succefully created css file in contact page');
+} catch (error) {
+  console.error('Error creating contact folder', error);
+}
+
+const aboutFolderPath = path.join(__dirname, 'about');
+try {
+  const indexFilePath = path.join(aboutFolderPath, 'index.html');
+  fs.writeFileSync(
+    indexFilePath,
+    '<!DOCTYPE html><html><head><title>About page</title><link rel= "stylesheet" href= "style.css"></head><body><h1>Hello, About us here</h1></body></html>'
+  );
+  console.log('Succesfully created index page in the about folder');
+
+  const cssFilePath = path.join(aboutFolderPath, 'style.css');
+  fs.writeFileSync(cssFilePath, 'body {background-color: red}');
+  console.log('Succefully created css file in about us page');
+} catch (error) {
+  console.error('error creating about folder');
+}
+
+const blogFolderPath = path.join(__dirname, 'blog');
+try {
+  const indexFilePath = path.join(blogFolderPath, 'index.html');
+  fs.writeFileSync(
+    indexFilePath,
+    '<!DOCTYPE html><html><head><title>Blog page</title><link rel= "stylesheet" href= "style.css"></head><body><h1>Hello, bloggers</h1></body></html>'
+  );
+  console.log('Succesfully created index page in the blog folder');
+
+  const cssFilePath = path.join(blogFolderPath, 'style.css');
+  fs.writeFileSync(cssFilePath, 'body {background-color: brown}');
+  console.log('Succefully created css file in blog us page');
+} catch (error) {
+  console.error('error creating blog folder');
+}
+
+//created the os
+const osName = os.arch();
+
+const infoTextFile = path.join(__dirname, 'info.txt');
+const infoTextContent = `This is being run on a ${osName} computer!`;
+
+try {
+  fs.writeFileSync(infoTextFile, infoTextContent, 'utf-8');
+  console.log('info.txt created in the client folder.');
+
+  // Read the 'info.txt' file
+  const content = fs.readFileSync(infoTextFile, 'utf-8');
+  console.log('File content:', content);
+} catch (error) {
+  console.error('Error creating info.txt file:', error.message);
+}
--- server.js
+++ server.js
@@ -1,70 +1,27 @@
+const http = require('http');
 const fs = require('fs');
 const path = require('path');
-const os = require('os');
 
-//creating files
-try {
-  const indexFilePath = path.join(__dirname, 'index.html');
-  fs.writeFileSync(
-    indexFilePath,
-    '<!DOCTYPE html><html><head><title>Homepage Client</title><link rel= "stylesheet" href= "style.css"></head><body><h1>Hello, World!</h1></body></html>'
-  );
-  console.log('Succesfully created index page');
+const server = http.createServer();
 
-  const cssFilePath = path.join(__dirname, 'style.css');
-  fs.writeFileSync(cssFilePath, 'body {background-color: pink}');
-  console.log('Succefully created css file');
-} catch (error) {
-  console.error('Error creating files', error);
-}
+server.on('request', (req, res) => {
+  const pathname = req.url === '/' ? '/index.html' : req.url + '/index.html';
 
-const contactFolderPath = path.join(__dirname, 'contact');
-try {
-  const indexFilePath = path.join(contactFolderPath, 'index.html');
-  fs.writeFileSync(
-    indexFilePath,
-    '<!DOCTYPE html><html><head><title>Contact page</title><link rel= "stylesheet" href= "style.css"></head><body><h1>Hello, Contact here!</h1></body></html>'
-  );
-  console.log('Succesfully created index page in the contact folder');
+  const fullPath = path.join(__dirname, pathname);
+  console.log('Full Path:', fullPath);
 
-  const cssFilePath = path.join(contactFolderPath, 'style.css');
-  fs.writeFileSync(cssFilePath, 'body {background-color: blue}');
-  console.log('Succefully created css file in contact page');
-} catch (error) {
-  console.error('Error creating contact folder', error);
-}
+  fs.readFile(fullPath, function (err, data) {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('File not found');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      return res.end();
+    }
+  });
+});
 
-const aboutFolderPath = path.join(__dirname, 'about');
-try {
-  const indexFilePath = path.join(aboutFolderPath, 'index.html');
-  fs.writeFileSync(
-    indexFilePath,
-    '<!DOCTYPE html><html><head><title>About page</title><link rel= "stylesheet" href= "style.css"></head><body><h1>Hello, About us here</h1></body></html>'
-  );
-  console.log('Succesfully created index page in the about folder');
-
-  const cssFilePath = path.join(aboutFolderPath, 'style.css');
-  fs.writeFileSync(cssFilePath, 'body {background-color: red}');
-  console.log('Succefully created css file in about us page');
-} catch (error) {
-  console.error('error creating about folder');
-}
-
-const blogFolderPath = path.join(__dirname, 'blog');
-try {
-  const indexFilePath = path.join(blogFolderPath, 'index.html');
-  fs.writeFileSync(
-    indexFilePath,
-    '<!DOCTYPE html><html><head><title>Blog page</title><link rel= "stylesheet" href= "style.css"></head><body><h1>Hello, bloggers</h1></body></html>'
-  );
-  console.log('Succesfully created index page in the blog folder');
-
-  const cssFilePath = path.join(blogFolderPath, 'style.css');
-  fs.writeFileSync(cssFilePath, 'body {background-color: brown}');
-  console.log('Succefully created css file in blog us page');
-} catch (error) {
-  console.error('error creating blog folder');
-}
-
-//created the os
-const osName = os.type();
+server.listen(3000, () => {
+  console.log('server started on http://127.0.0.1:3000');
+});
